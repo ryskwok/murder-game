a6a1201769cdcfa1be332a0efc41d5355a344f51
Ryan Kwok <ryskwok@meta.com>
2025-05-11 18:32:05 -0700

    Update build configuration for proper MIME types

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
