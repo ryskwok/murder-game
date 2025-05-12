71995d9bc4bf589ed8f08cfbea64937bb62b6376
Ryan Kwok <ryskwok@meta.com>
2025-05-11 18:42:01 -0700

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/murder-game/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].mjs',
-        chunkFileNames: 'assets/[name].mjs',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
       }
     }
